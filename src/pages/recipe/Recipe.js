598b3e62e682f4718f77a53980947c0757404bb6
Daniel Son <dson.email@gmail.com>
2022-05-22 14:50:57 +1000

    Show selected recipe

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,22 +1,29 @@
 import "./Recipe.css";
-import {useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import { useEffect } from "react/cjs/react.production.min";
 
 export default function Recipe() {
-    const { id } = useParams();
-    const url = "http://localhost:3000/recipes/" + id;
-    const { data: recipe, isPending, error } = useFetch(url);
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data: recipe, isPending, error } = useFetch(url);
 
-    return (
-        <div className="recipe">
-          {isPending && <p className="error">Loading...</p>}
-          {error && <p className="error">{error}</p>}
-          {recipe && (
-            <div>
-              <h2>{recipe.title}</h2>
-            </div>
-          )}
-        </div>
-      );
+  return (
+    <div className="recipe">
+      {isPending && <p className="error">Loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
