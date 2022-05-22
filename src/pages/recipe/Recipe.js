7fe128e7baa58bdab635a1f002e5a5b9bc862c64
Daniel Son <dson.email@gmail.com>
2022-05-22 14:46:33 +1000

    Show routed recipes

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,7 +1,22 @@
 import "./Recipe.css";
+import {useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import { useEffect } from "react/cjs/react.production.min";
 
 export default function Recipe() {
-  return (
-    <div>Recipe</div>
-  )
+    const { id } = useParams();
+    const url = "http://localhost:3000/recipes/" + id;
+    const { data: recipe, isPending, error } = useFetch(url);
+
+    return (
+        <div className="recipe">
+          {isPending && <p className="error">Loading...</p>}
+          {error && <p className="error">{error}</p>}
+          {recipe && (
+            <div>
+              <h2>{recipe.title}</h2>
+            </div>
+          )}
+        </div>
+      );
 }
