35c7d6b50c5c5a3328752e3444a7d710a22362e7
Daniel Son <dson.email@gmail.com>
2022-05-25 19:30:52 +1000

    Search using searchbar and display results

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,7 +1,22 @@
 import "./Search.css";
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import RecipesList from "../../components/RecipeList";
 
 export default function Search() {
+  const queryString = useLocation().search; // .search is a property from react-router-dom. It's the query string part after the ?.
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("query");
+
+  const url = `http://localhost:3000/recipes?q=${query}`;
+  const { error, isPending, data: recipes } = useFetch(url);
+
   return (
-    <div>Search</div>
-  )
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipes && <RecipesList recipes={recipes} />}
+    </div>
+  );
 }
